639f8c690ebcedcdb5f9657d288e04abed4d847c
Atr07 <adithyaatr@gmail.com>
2019-11-29 14:12:01 +0530

    not-random-key

--- app/src/main/java/com/test/exoplayer2/MainActivity.java
+++ app/src/main/java/com/test/exoplayer2/MainActivity.java
@@ -1,8 +1,20 @@
 package com.test.exoplayer2;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.os.EnvironmentCompat;
+
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
@@ -35,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
   public static final String AES_TRANSFORMATION = "AES/CTR/NoPadding";
 
   private static final String ENCRYPTED_FILE_NAME = "BigBuckBunnyEncrypted.exo.enc";
+  private static final String FILE_NAME = "/BigBuckBunnyEncrypted.exo.enc";
   private static final String URL_1 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
   private static final String URL_2 = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4";
 
@@ -59,10 +72,10 @@ public class MainActivity extends AppCompatActivity {
     Log.d(getClass().getCanonicalName(), getFilesDir().getAbsolutePath());
 
     SecureRandom secureRandom = new SecureRandom();
-    byte[] key = new byte[16];
-    byte[] iv = new byte[16];
-    secureRandom.nextBytes(key);
-    secureRandom.nextBytes(iv);
+    final byte[] iv = { 65, 1, 2, 23, 4, 5, 6, 7, 32, 21, 10, 11, 12, 13, 84, 45 };
+    final byte[] key = { 0, 42, 2, 54, 4, 45, 6, 7, 65, 9, 54, 11, 12, 13, 60, 15 };
+    /*secureRandom.nextBytes(key);
+    secureRandom.nextBytes(iv);*/
 
     mSecretKeySpec = new SecretKeySpec(key, AES_ALGORITHM);
     mIvParameterSpec = new IvParameterSpec(iv);
@@ -96,6 +109,13 @@ public class MainActivity extends AppCompatActivity {
       // the ciphers, key and iv used in this demo, or to see it from top to bottom,
       // supply a url to a remote unencrypted file - this method will download and encrypt it
       // this first argument needs to be that url, not null or empty...
+      /*int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+      if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+      } else {
+        new DownloadAndEncryptFileTask(URL_2, mEncryptedFile, encryptionCipher).execute();
+      }*/
+
       new DownloadAndEncryptFileTask(URL_2, mEncryptedFile, encryptionCipher).execute();
     } catch (Exception e) {
       e.printStackTrace();
@@ -121,4 +141,33 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  public void choosePath(View view) {
+    Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+    i.addCategory(Intent.CATEGORY_DEFAULT);
+    startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 9999) {
+      assert data != null;
+      Log.i("Test", "Result URI " + data.getData());
+
+      String path = FileUtil.getFullPathFromTreeUri(data.getData(), this);
+      Log.i("Test", "Path " + data.getData());
+
+      //mEncryptedFile = new File("/storage/emulated/0/score_videos", FILE_NAME);
+      mEncryptedFile = new File("/storage/6563-3130/Download", FILE_NAME);
+
+      String state = EnvironmentCompat.getStorageState(mEncryptedFile);
+      Log.i("Test", "getStorageState " + EnvironmentCompat.getStorageState(mEncryptedFile));
+      if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+        Log.i("Test", "Read only ");
+      } else {
+        Log.i("Test", "Full access");
+      }
+    }
+  }
 }
